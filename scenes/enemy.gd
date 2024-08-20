07e8bf01872ed39242cdc6c2792f63e034fd51e2
loheesong <loheesong@gmail.com>
2024-08-20 15:53:52 +0800

    player kill itself

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -15,11 +15,10 @@ func _process(delta: float) -> void:
 func _on_area_entered(area: Area2D) -> void:
 	if area.name == "Player":
 		print("Collided lol")
-
+		area.queue_free()
 
 func _on_player_moved(pos: Vector2) -> void:
 	dest = pos
-
 
 func _on_enemy_turn_started() -> void:
 	move_towards_player()
